47eb16f6bdea44a7b4f7c6ba42f14f61b900df9e
Jacob Froman <jacob.h.froman@gmail.com>
2016-11-02 18:16:11 -0700

    Updated to version 0.9

--- launcher.py
+++ launcher.py
@@ -11,7 +11,7 @@ class Launcher(Ui_Launcher):
         self.setupUi(dialog)
         dialog.setWindowTitle("MB EDI Launcher")
         dialog.setWindowIcon(QtGui.QIcon("Resources\\MBIcon.bmp"))
-        self.VersionLabel.setText("Beta 0.5.7 9-5-2016")
+        self.VersionLabel.setText("Beta 0.9 11-2-2016")
         self.ShipButton.clicked.connect(self.ship_clicked)
         self.POButton.clicked.connect(self.po_clicked)
 
